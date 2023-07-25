9b34872ba05c56de287e6b44c115c6d3fb17931c
eryk-poradecki <eryk.poradecki@gmail.com>
2023-07-25 20:40:02 +0200

    feat: modified home view to display popout modals

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,8 +21,8 @@
 </template> -->
 
 <template>
-  <div v-for="category in categories" :key="category">
-    <h1 class="text-3xl font-bold text-center pt-10">
+  <div v-for="category in categories" :key="category" class="pt-10">
+    <h1 class="text-3xl font-bold text-center text-white">
       {{ categoryNames[categories.indexOf(category)] }}
     </h1>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8 max-w-[85%] mx-auto">
@@ -36,26 +36,49 @@
             :src="project.image"
             :alt="project.name"
             class="rounded-t-xl w-full h-72 object-cover"
+            @click="openModal(project)"
+            style="cursor: pointer"
           />
           <div class="px-3 py-3">
-            <h1 class="font-bold text-xl">{{ project.name }}</h1>
-            <p class="text-md text-gray-900">{{ project.shortDescription }}</p>
+            <div class="flex justify-center items-center mb-1">
+                <h1 class="font-bold text-2xl">{{ project.name }}</h1>
+            </div>
+            <p class="text-lg text-gray-900">{{ project.shortDescription }}</p>
           </div>
         </div>
       </div>
     </div>
   </div>
+
+  <teleport to="body">
+    <PopoutModal v-if="isModalOpen" :closeModal="closeModal" :selectedProject="selectedProject" />
+  </teleport>
 </template>
 
 <script setup>
 import { ref, onMounted, computed } from "vue";
 import projectsJson from "../assets/projects.json";
+import PopoutModal from "../components/PopoutModal.vue";
 
 const categories = ["game", "music", "asset"];
 const categoryNames = ["Games", "Music", "Assets"];
+
 const projects = ref([]);
+const selectedProject = ref(null);
+
+const isModalOpen = ref(false);
 
 onMounted(() => {
   projects.value = projectsJson;
 });
+
+const openModal = (project) => {
+  selectedProject.value = project; // Set the selected project when the modal is opened
+  isModalOpen.value = true;
+};
+
+const closeModal = () => {
+  isModalOpen.value = false;
+};
+
 </script>
