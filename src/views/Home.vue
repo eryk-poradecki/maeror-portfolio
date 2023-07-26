6c1e668e5585afe78211252c7bd3c8690c5bb219
eryk-poradecki <eryk.poradecki@gmail.com>
2023-07-26 14:04:23 +0200

    feat: home view category filter + modal fixes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,8 +21,8 @@
 </template> -->
 
 <template>
-  <div v-for="category in categories" :key="category" class="pt-10">
-    <h1 class="text-3xl font-bold text-center text-white">
+  <div v-for="category in filteredCategories" :key="category" class="pt-10">
+    <h1 v-if="hasProjectsInCategory(category)" class="text-3xl font-bold text-center text-neutral-200">
       {{ categoryNames[categories.indexOf(category)] }}
     </h1>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8 max-w-[85%] mx-auto">
@@ -31,7 +31,7 @@
         :key="project.id"
         class="bg-neutral-400 shadow-lg rounded-xl max-w-md mx-auto"
       >
-        <div v-if="project.category == category">
+        <div v-if="project.category === category">
           <img
             :src="project.image"
             :alt="project.name"
@@ -41,7 +41,7 @@
           />
           <div class="px-3 py-3">
             <div class="flex justify-center items-center mb-1">
-                <h1 class="font-bold text-2xl">{{ project.name }}</h1>
+              <h1 class="font-bold text-2xl">{{ project.name }}</h1>
             </div>
             <p class="text-lg text-gray-900">{{ project.shortDescription }}</p>
           </div>
@@ -51,29 +51,31 @@
   </div>
 
   <teleport to="body">
-    <PopoutModal v-if="isModalOpen" :closeModal="closeModal" :selectedProject="selectedProject" />
+    <transition name="fade">
+      <PopupModal v-if="isModalOpen" :closeModal="closeModal" :selectedProject="selectedProject" />
+    </transition>
   </teleport>
 </template>
 
 <script setup>
 import { ref, onMounted, computed } from "vue";
 import projectsJson from "../assets/projects.json";
-import PopoutModal from "../components/PopoutModal.vue";
+import PopupModal from "../components/PopupModal.vue";
 
 const categories = ["game", "music", "asset"];
 const categoryNames = ["Games", "Music", "Assets"];
 
 const projects = ref([]);
-const selectedProject = ref(null);
-
-const isModalOpen = ref(false);
 
 onMounted(() => {
   projects.value = projectsJson;
 });
 
+const isModalOpen = ref(false);
+const selectedProject = ref(null);
+
 const openModal = (project) => {
-  selectedProject.value = project; // Set the selected project when the modal is opened
+  selectedProject.value = project;
   isModalOpen.value = true;
 };
 
@@ -81,4 +83,14 @@ const closeModal = () => {
   isModalOpen.value = false;
 };
 
+const hasProjectsInCategory = (category) => {
+  return projects.value.some((project) => project.category === category);
+};
+
+const filteredCategories = computed(() => {
+  return categories.filter((category) => hasProjectsInCategory(category));
+});
 </script>
+<style>
+
+</style>
