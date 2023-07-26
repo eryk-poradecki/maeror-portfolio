52dfec08a0d755c5d5acfaa44dad7ef97b431a48
eryk-poradecki <eryk.poradecki@gmail.com>
2023-07-26 18:22:06 +0200

    feat: added a bandcamp link

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen">
+  <div class="flex flex-col items-center justify-center">
     <div
-      class="border-3 custom-border-gradient shadow-lg rounded-xl w-[60%] min-h-[40vh] flex flex-col items-center justify-center"
+      class="shadow-lg rounded-xl w-[60%] mt-[5%] pt-[10%] flex flex-col items-center justify-center"
     >
       <div class="pb-[15%]">
         <!-- TEXT ON THE RIGHT OF THE AVATAR -->
-        <!-- <div class="flex items-center">
+        <div class="flex items-center">
           <img
             class="w-24 h-24 rounded-full mx-auto"
             src="src/assets/maeror-avatar.png"
@@ -14,8 +14,8 @@
             height="512"
           />
           <h1 class="text-4xl font-bold text-white ml-4">Maeror</h1>
-        </div> -->
-        <div class="flex flex-col items-center">
+        </div>
+        <!-- <div class="flex flex-col items-center">
           <img
             class="w-24 h-24 rounded-full"
             src="src/assets/maeror-avatar.png"
@@ -23,8 +23,8 @@
             width="384"
             height="512"
           />
-          <h1 class="text-4xl font-bold text-neutral-200 mt-4">Maeror</h1>
-        </div>
+          <h1 class="text-4xl font-bold text-neutral-300 mt-4">Maeror</h1>
+        </div> -->
 
         <div class="flex mt-5 justify-center items-center">
           <a
@@ -60,6 +60,17 @@
               class="w-8 h-8 mt-5 ml-4"
             />
           </a>
+          <a
+            href="https://maerormusic.bandcamp.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="src/assets/svg/bandcamp-mark.svg"
+              alt="Bandcamp"
+              class="w-8 h-8 mt-5 ml-4"
+            />
+          </a>
         </div>
       </div>
     </div>
@@ -67,8 +78,4 @@
 </template>
 
 <style>
-.custom-border-gradient {
-  border-image: linear-gradient(135deg, #28313b 100%, #485461 100%);
-  border-image-slice: 1;
-}
 </style>
