156783d958c3b062bdf33b9d9a3c4735571ecb46
eryk-poradecki <eryk.poradecki@gmail.com>
2023-07-28 19:03:04 +0200

    feat: added routing for the resume url

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import Resume from '../views/Resume.vue'
 
 const routes = [
     {
@@ -13,6 +14,11 @@ const routes = [
         name: 'about',
         component: About,
     },
+    {
+        path: '/resume',
+        name: 'resume',
+        component: Resume,
+    }
 ]
 
 const router = createRouter({
